1c7a3a2255e47983b27c48817777938141399ba4
Jamal Frank <jamaljfrank@gmail.com>
2020-04-02 22:36:35 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -7,4 +7,10 @@ class ArtistsController < ApplicationController
   def new 
     @artist = Artist.new 
   end
+  
+  def create
+    @student = Student.new(student_params(:first_name, :last_name))
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
